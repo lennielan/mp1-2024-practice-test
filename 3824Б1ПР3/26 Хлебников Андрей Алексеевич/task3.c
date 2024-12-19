21b3bc5c02f48c3025ecd7de2762244e6115e31a
lennielan <andreykhlebnikov2006@gmail.com>
2024-12-19 10:42:09 +0300

    3824Б1ПР3_Хлебников.c

--- 3824Б1ПР3/26 Хлебников Андрей Алексеевич/task3.c
+++ 3824Б1ПР3/26 Хлебников Андрей Алексеевич/task3.c
@@ -3,5 +3,18 @@
 // Пример: s = "abc1d2e34f56g", ответ: "123456".
 char* task3(char *s)
 {
-  return "task3";
+  int len = 0, j = 0;
+  for(int i = 0; s[i] != '\0'; i++){
+    if(isdigit((unsigned char)input[i])) {
+         len++; 
+      }
+  }
+  char *s1 = (char *)malloc((len + 1) * sizeof(char));
+  for(int i = 0; s[i] != '\0'; i++){
+    if(isdigit((unsigned char)input[i])) {
+         s1[j++] = s[i]; 
+      }
+  }
+  s1[j] = '\0';
+  return s1;
 }
